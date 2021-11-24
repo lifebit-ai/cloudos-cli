21a89d7ed2e17d90a08d001bca1c6f3a78fa5310
ilevantis <ilevantis@users.noreply.github.com>
2021-11-24 16:11:16 +0000

    change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cloudos",
-    version="0.0.4",
+    version="0.0.5b",
     author="David Piñeyro",
     author_email="dapineyro.dev@gmail.com",
     description="Python package for interacting with CloudOS",
