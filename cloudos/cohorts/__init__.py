8c1095e8c5b1f0fbd4590074db1ddb4912d57f31
ilevantis <ilevantis@users.noreply.github.com>
2021-12-03 13:28:58 +0000

    update init

--- cloudos/cohorts/__init__.py
+++ cloudos/cohorts/__init__.py
@@ -4,6 +4,6 @@ Functions and classes related to cohorts.
 
 from .cohort import Cohort
 from .cohort_browser import CohortBrowser
+from .query import Phenotype, Query
 
-
-__all__ = ['cohort', 'cohort_browser']
+__all__ = ['cohort', 'cohort_browser', 'query']
