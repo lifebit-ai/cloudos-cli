cc86fb8a3f0f6306d06780129625fc8641f063e1
ewartj <ewart.jonny@gmail.com>
2021-12-16 17:50:25 +0000

    added function to get phenotype metadata (#18)
    * added function to get phenotype metadata
    
    * added docstring
    
    * added newline at end of file to make pep8 compliant
    
    * added blank line to match existing docstring format
    
    Co-authored-by: ilevantis <ilevantis@users.noreply.github.com>
    
    * Remove extra spaces
    
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>
    
    * Correct indentation
    
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>
    
    * Remove blank end lines
    
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>
    
    * added empty line between docstring and the Parameters section of the docstring
    
    Co-authored-by: ilevantis <ilevantis@users.noreply.github.com>
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>

--- cloudos/cohorts/cohort_browser.py
+++ cloudos/cohorts/cohort_browser.py
@@ -1,9 +1,10 @@
 """
 This is the main class for interacting with a cohort browser instance.
 """
-
+import requests
 from dataclasses import dataclass
 from cloudos.cohorts import Cohort
+from cloudos.utils.errors import BadRequestException
 
 
 @dataclass
@@ -56,3 +57,27 @@ class CohortBrowser:
         """
         return Cohort.create(self.apikey, self.cloudos_url, self.workspace_id,
                              cohort_name, cohort_desc=cohort_desc)
+
+    def get_phenotype_metadata(self, pheno_id):
+        """Get metadata on a phenotype.
+
+        Parameters
+        ----------
+        pheno_id : int
+            The id of the phenotype of interest.
+
+        Returns
+        -------
+        Dict
+        """
+        headers = {"apikey": self.apikey,
+                   "Accept": "application/json, text/plain, */*",
+                   "Content-Type": "application/json;charset=UTF-8"}
+        params = {"teamId": self.workspace_id}
+        r = requests.get(f"{self.cloudos_url}/cohort-browser/v2/cohort/filter/{pheno_id}/metadata",
+                         params=params, headers=headers)
+        if r.status_code >= 400:
+            raise BadRequestException(r)
+        r_json = r.json()
+        r_json.pop('_id', None)
+        return r_json
