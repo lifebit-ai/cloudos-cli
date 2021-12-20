f3663f50627a5860676e9fb879391187a4494750
ewartj <ewart.jonny@gmail.com>
2021-12-20 15:24:41 +0000

    added function to list cohorts

--- cloudos/cohorts/cohort_browser.py
+++ cloudos/cohorts/cohort_browser.py
@@ -81,3 +81,40 @@ class CohortBrowser:
         r_json = r.json()
         r_json.pop('_id', None)
         return r_json
+
+    def list_cohorts(self, size=10):
+        """List all cohorts on the first page of Cohort browser.
+
+        Parameters
+        ----------
+        term : int
+            Number of cohorts to list on the first page.
+
+        Returns
+        -------
+        Dict
+        """
+        headers = {"apikey": self.apikey,
+                   "Accept": "application/json, text/plain, */*",
+                   "Content-Type": "application/json;charset=UTF-8"}
+        params = {"teamId": self.workspace_id,
+                  "pageNumber": 0,
+                  "pageSize": size}
+        r = requests.get(f"{self.cloudos_url}/cohort-browser/v2/cohort",
+                         params=params, headers=headers)
+        if r.status_code >= 400:
+            raise BadRequestException(r)
+        r_json = r.json()
+        if size == 10:
+            print(f"""Total number of cohorts found: {r_json['total']}. 
+            Showing {size} by default. Change 'size' parameter to return more.
+            """.strip().replace("            ", ""))
+        values_to_take = ["name", "description", "numberOfParticipants",
+                          "numberOfFilters", "createdAt", "updatedAt"]
+        cohort_dict = {}
+        for i in range(len(r_json['cohorts'])):
+            r_json['cohorts'][i]['numberOfFilters'] = len(r_json['cohorts'][i]['phenotypeFilters'])
+            temp_dict = {x: r_json['cohorts'][i][x] for x in r_json['cohorts'][i]
+                         if x in values_to_take}
+            cohort_dict[i] = temp_dict
+        return cohort_dict
