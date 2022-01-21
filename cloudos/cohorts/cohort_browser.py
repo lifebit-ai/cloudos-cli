be4e7cc4e3506690317b2d998f0901bd5cefae47
ewartj <ewart.jonny@gmail.com>
2022-01-21 16:17:45 +0000

    updated cohort_browser so in line with dev

--- cloudos/cohorts/cohort_browser.py
+++ cloudos/cohorts/cohort_browser.py
@@ -81,3 +81,97 @@ class CohortBrowser:
         r_json = r.json()
         r_json.pop('_id', None)
         return r_json
+
+    def list_cohorts(self, size=10):
+        """List all cohorts from the first page of Cohort browser.
+
+        Parameters
+        ----------
+        term : int or String "all"
+            Number of cohorts to list from the first page.
+
+        Returns
+        -------
+        Dict
+        """
+        headers = {"apikey": self.apikey,
+                   "Accept": "application/json, text/plain, */*",
+                   "Content-Type": "application/json;charset=UTF-8"}
+        if size == "all":
+            size = 0
+            params = {"teamId": self.workspace_id,
+                      "pageNumber": 0,
+                      "pageSize": size}
+            r = requests.get(f"{self.cloudos_url}/cohort-browser/v2/cohort",
+                             params=params, headers=headers)
+            if r.status_code >= 400:
+                raise BadRequestException(r)
+            r_json = r.json()
+            size = r_json['total']
+        params = {"teamId": self.workspace_id,
+                  "pageNumber": 0,
+                  "pageSize": size}
+        r = requests.get(f"{self.cloudos_url}/cohort-browser/v2/cohort",
+                            params=params, headers=headers)
+        if r.status_code >= 400:
+            raise BadRequestException(r)
+        r_json = r.json()
+        if size > r_json['total']:
+            size = r_json['total']
+        if size == 10:
+            print(f"""Total number of cohorts found: {r_json['total']}.
+            Showing {size} by default. Change 'size' parameter to return more.
+            """.strip().replace("            ", ""))
+        else:
+            print(f"Total number of cohorts found: {r_json['total']}. Showing: {size}.")
+        values_to_take = ["name", "_id", "description", "numberOfParticipants",
+                          "numberOfFilters", "createdAt", "updatedAt"]
+        cohort_list = []
+        for cohort in r_json['cohorts']:
+            temp_item = {k: v for k, v in cohort.items() if k in values_to_take}
+            temp_item['numberOfFilters'] = len(cohort['phenotypeFilters'])
+            cohort_list.append(temp_item)
+        return cohort_list
+
+    def search_phenotypes(self, term='', all_metadata=False):
+        """Search for phenotypes with 'term' in their name.
+
+        Parameters
+        ----------
+        term : string. Default=''
+            The string with which to search for phenotypes. Empty string will return
+            all phenotypes.
+        all_metadata: boolean. Default=False
+            Set to True to return all metadata for each returned phenotype.
+        Returns
+        -------
+        Dict
+        """
+        headers = {"apikey": self.apikey,
+                   "Accept": "application/json, text/plain, */*",
+                   "Content-Type": "application/json;charset=UTF-8"}
+        params = {"term": term,
+                  "teamId": self.workspace_id}
+        r = requests.get(f"{self.cloudos_url}/cohort-browser/v2/cohort/fields_search",
+                         params=params, headers=headers)
+        if r.status_code >= 400:
+            raise BadRequestException(r)
+        r_json = r.json()
+        print(f"Total number of phenotypes found - {len(r_json['filters'])}")
+        if all_metadata is True:
+            return r_json['filters']
+        else:
+            values_to_take = ["id", "categoryPathLevel1", "categoryPathLevel2",
+                              "categoryPathLevel3", "name", "description",
+                              "type", "valueType", "units", "display",
+                              "possibleValues", "min", "max", "recruiterDescription",
+                              "group", "clinicalForm", "parent", "instances", "array",
+                              "Sorting", "coding", "descriptionParticipantsNo",
+                              "link", "descriptionCategoryID", "descriptionItemType",
+                              "descriptionStrata", "descriptionSexed"]
+            phenotypes_list = []
+            for i, item in enumerate(r_json['filters']):
+                temp_item = {item: r_json['filters'][i][item] for item in r_json['filters'][i]
+                             if item in values_to_take}
+                phenotypes_list.append(temp_item)
+            return phenotypes_list
