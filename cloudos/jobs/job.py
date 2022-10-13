18689cbb38192bcd5fcd8cafe5021a360c3c7139
dapineyro <dapineyro.dev@gmail.com>
2022-10-13 17:12:10 +0200

    simplify code 2

--- cloudos/jobs/job.py
+++ cloudos/jobs/job.py
@@ -140,11 +140,9 @@ class Job(Cloudos):
         for element in json.loads(r.content):
             if resource == 'workflows':
                 if (element["name"] == name and element["repository"]["platform"] == repository_platform):
-                    # For GitHub repos always select the first workflow available
                     if mainfile is None:
                         return element["_id"]
                     if element["mainFile"] == mainfile:
-                        # WDL workflows do not have importsFile field when not used.
                         if importsfile is None and "importsFile" not in element.keys():
                             return element["_id"]
                         elif "importsFile" in element.keys() and element["importsFile"] == importsfile:
