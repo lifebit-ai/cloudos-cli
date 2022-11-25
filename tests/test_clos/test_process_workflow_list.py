18e87ef5b67fc26ad58fabb2d8896fdd5e4f48df
egenc <erdal.genc09@gmail.com>
2022-11-25 12:42:18 +0300

    Apply suggestions from code review
    Co-authored-by: Daniel Boloc <danielboloc@users.noreply.github.com>

--- tests/test_clos/test_process_workflow_list.py
+++ tests/test_clos/test_process_workflow_list.py
@@ -2,13 +2,11 @@ import requests, json
 from cloudos.clos import Cloudos
 import pytest
 import requests_mock
-
 import pandas as pd
 
 INPUT_JSON = "tests/test_data/process_workflow_list_initial_request.json"
 output_df = pd.read_csv("tests/test_data/process_workflow_list_results.csv")
 output_df_full = pd.read_csv("tests/test_data/process_workflow_list_results_FULL.csv")
-
 
 @pytest.fixture()
 def mocked_requests_get():
@@ -21,10 +19,12 @@ def mocked_requests_get():
         r = requests.get(f"http://test_cloud_os/api/v1/jobs?teamId={test_workspace_id}")
     return r
 
-def test_process_workflow_list(mocked_requests_get):
+def test_process_workflow_list_all_fields_false(mocked_requests_get):
+    """Test function parameter 'all_fields=False'"""
     df = Cloudos.process_workflow_list(mocked_requests_get, all_fields=False)
     assert df.equals(output_df)
 
-def test_process_workflow_list_all_fields(mocked_requests_get):
+def test_process_workflow_list_all_fields_true(mocked_requests_get):
+    """Test function parameter 'all_fields=True'"""
     df = Cloudos.process_workflow_list(mocked_requests_get, all_fields=True)
     assert df.equals(output_df_full)
