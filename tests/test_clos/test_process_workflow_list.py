e8421148cf800c857b049d9be4068ff21b7e1f9e
egenc <erdal.genc09@gmail.com>
2022-11-28 11:20:59 +0300

    Apply suggestions from code review
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>

--- tests/test_clos/test_process_workflow_list.py
+++ tests/test_clos/test_process_workflow_list.py
@@ -1,4 +1,5 @@
-import requests, json
+import requests
+import json
 from cloudos.clos import Cloudos
 import pytest
 import requests_mock
@@ -7,6 +8,7 @@ import pandas as pd
 INPUT_JSON = "tests/test_data/process_workflow_list_initial_request.json"
 output_df = pd.read_csv("tests/test_data/process_workflow_list_results.csv")
 output_df_full = pd.read_csv("tests/test_data/process_workflow_list_results_FULL.csv")
+
 
 @pytest.fixture()
 def mocked_requests_get():
@@ -19,10 +21,12 @@ def mocked_requests_get():
         r = requests.get(f"http://test_cloud_os/api/v1/jobs?teamId={test_workspace_id}")
     return r
 
+
 def test_process_workflow_list_all_fields_false(mocked_requests_get):
     """Test function parameter 'all_fields=False'"""
     df = Cloudos.process_workflow_list(mocked_requests_get, all_fields=False)
     assert df.equals(output_df)
+
 
 def test_process_workflow_list_all_fields_true(mocked_requests_get):
     """Test function parameter 'all_fields=True'"""
