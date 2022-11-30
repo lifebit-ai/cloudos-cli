5fe390f265ad2438243fb8473609d4ac23d5f51e
egenc <erdal.genc09@gmail.com>
2022-11-30 14:36:03 +0300

    Apply suggestions from code review
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>

--- tests/test_clos/test_detect_workflow.py
+++ tests/test_clos/test_detect_workflow.py
@@ -9,7 +9,8 @@ APIKEY = 'vnoiweur89u2ongs'
 CLOUDOS_URL = 'http://cloudos.lifebit.ai'
 WORKSPACE_ID = 'lv89ufc838sdig'
 
-EXPECTED_WORKFLOWS_LIST = ["nextflow" , "docker" , "wdl"]
+EXPECTED_WORKFLOWS_LIST = ["nextflow", "docker", "wdl"]
+
 
 @mock.patch('cloudos.clos', mock.MagicMock())
 @responses.activate
@@ -18,11 +19,7 @@ def test_detect_workflow():
     Test 'get_workflow_list' to work as intended
     API request is mocked and replicated with json files
     """
-    with open(INPUT) as json_data:
-        data = json.load(json_data)
-        json_data = json.dumps(data)
-
-
+    json_data = load_json_file(INPUT)
     params = {"teamId": WORKSPACE_ID, "apikey": APIKEY}
     header = {
         "Accept": "application/json, text/plain, */*",
@@ -37,8 +34,8 @@ def test_detect_workflow():
             headers=header,
             match=[matchers.query_param_matcher(params)],
             status=200)
-    # start cloudOS service 
+    # start cloudOS service
     clos = Cloudos(apikey=APIKEY, cromwell_token=None, cloudos_url=CLOUDOS_URL)
     # get mock response
     response = clos.detect_workflow(workspace_id=WORKSPACE_ID, workflow_name="picard")
-    assert response in EXPECTED_WORKFLOWS_LIST
+    assert response == 'docker'
