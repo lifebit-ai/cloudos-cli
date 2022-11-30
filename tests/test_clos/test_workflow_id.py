8929969bec517598a4a28e71c683bd24e0bd7a86
Erdal Genc <erdalgenc@Erdals-MacBook-Pro.local>
2022-11-30 18:49:46 +0300

    Object Class is modified to Job

--- tests/test_clos/test_workflow_id.py
+++ tests/test_clos/test_workflow_id.py
@@ -1,18 +1,18 @@
 import mock
 import json
-import pytest
 import requests
 import responses
 from responses import matchers
-from cloudos.clos import Cloudos
-from cloudos.utils.errors import BadRequestException
+from cloudos.jobs import Job
 from tests.functions_for_pytest import load_json_file
 
-INPUT = "tests/test_data/process_job_list_initial_json.json"
+INPUT_PROJECT = "tests/test_data/projects.json"
+INPUT_WORKFLOW = "tests/test_data/workflows.json"
 APIKEY = 'vnoiweur89u2ongs'
 CLOUDOS_URL = 'http://cloudos.lifebit.ai'
 WORKSPACE_ID = 'lv89ufc838sdig'
-
+PROJECT_NAME = "anastasios-testing"
+WORKFLOW_NAME = "nf-core-deepvariant"
 
 @mock.patch('cloudos.clos', mock.MagicMock())
 @responses.activate
@@ -21,7 +21,8 @@ def test_workflow_id():
     Test 'workflow_id' to work as intended
     API request is mocked and replicated with json files
     """
-    create_json = load_json_file(INPUT)
+    create_json_project = load_json_file(INPUT_PROJECT)
+    create_json_workflow = load_json_file(INPUT_WORKFLOW)
     params = {"teamId": WORKSPACE_ID, "apikey": APIKEY}
     header = {
         "Accept": "application/json, text/plain, */*",
@@ -31,16 +32,26 @@ def test_workflow_id():
     # mock GET method with the .json
     responses.add(
             responses.GET,
-            url=f"{CLOUDOS_URL}/api/v1/jobs?{search_str}",
-            body=create_json,
+            url=f"{CLOUDOS_URL}/api/v1/projects?{search_str}",
+            body=create_json_project,
             headers=header,
             match=[matchers.query_param_matcher(params)],
             status=200)
+    responses.add(
+            responses.GET,
+            url=f"{CLOUDOS_URL}/api/v1/workflows?{search_str}",
+            body=create_json_workflow,
+            headers=header,
+            match=[matchers.query_param_matcher(params)],
+            status=201)
     # start cloudOS service
-    clos = Cloudos(apikey=APIKEY, cromwell_token=None, cloudos_url=CLOUDOS_URL)
+    job = Job(apikey=APIKEY,
+        cloudos_url=CLOUDOS_URL,
+        workspace_id=WORKSPACE_ID,
+        cromwell_token=None,
+        project_name=PROJECT_NAME,
+        workflow_name=WORKFLOW_NAME)
     # get mock response
-    response = clos.get_job_list(WORKSPACE_ID)
-    workflow_id = response.json()["jobs"][0]["workflow"]["_id"]
+    wf_id = job.workflow_id
     # check the response
-    assert response.status_code == 200
-    assert isinstance(workflow_id, str) and len(workflow_id) > 0
+    assert isinstance(wf_id, str) and len(wf_id) > 0
