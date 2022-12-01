39fcca7f90adb8720cbb073a9bea6ef86d225e56
Erdal Genc <erdalgenc@Erdals-MacBook-Pro.local>
2022-12-01 18:08:55 +0300

    code improvement

--- tests/test_clos/test_process_workflow_list.py
+++ tests/test_clos/test_process_workflow_list.py
@@ -1,5 +1,6 @@
-import requests
+"""pytest for fucntion process_workflow_list"""
 import json
+import requests
 from cloudos.clos import Cloudos
 import pytest
 import requests_mock
@@ -10,25 +11,27 @@ output_df = pd.read_csv("tests/test_data/process_workflow_list_results.csv")
 output_df_full = pd.read_csv("tests/test_data/process_workflow_list_results_FULL.csv")
 
 
-@pytest.fixture()
-def mocked_requests_get():
+@pytest.fixture(name="mocked_requests_get")
+def fixture_mocked_requests_get():
+    """Creating a mock request"""
     test_workspace_id = 1
-    with open(INPUT_JSON) as json_data:
-        d = json.load(json_data)
+    with open(INPUT_JSON, encoding="utf-8") as json_data:
+        data_d = json.load(json_data)
     with requests_mock.Mocker() as mock:
         mock.get(f"http://test_cloud_os/api/v1/jobs?teamId={test_workspace_id}",
-                 json=d)
-        r = requests.get(f"http://test_cloud_os/api/v1/jobs?teamId={test_workspace_id}")
-    return r
+                 json=data_d)
+        r_get = requests.get(f"http://test_cloud_os/api/v1/jobs?teamId={test_workspace_id}")
+    return r_get
 
 
 def test_process_workflow_list_all_fields_false(mocked_requests_get):
     """Test function parameter 'all_fields=False'"""
-    df = Cloudos.process_workflow_list(mocked_requests_get, all_fields=False)
-    assert (df.columns == output_df.columns).any()
+    df_ = Cloudos.process_workflow_list(mocked_requests_get, all_fields=False)
+    assert (df_.columns == output_df.columns).any()
 
 
 def test_process_workflow_list_all_fields_true(mocked_requests_get):
     """Test function parameter 'all_fields=True'"""
-    df = Cloudos.process_workflow_list(mocked_requests_get, all_fields=True)
-    assert (df.columns == output_df_full.columns).any()
+    df_ = Cloudos.process_workflow_list(mocked_requests_get, all_fields=True)
+    assert (df_.columns == output_df_full.columns).any()
+    
