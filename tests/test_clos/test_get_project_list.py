6c580867683dab2da584eadda0b05cfc101b9088
dapineyro <dapineyro.dev@gmail.com>
2023-04-04 19:07:34 +0200

    add tests

---
+++ tests/test_clos/test_get_project_list.py
@@ -0,0 +1,72 @@
+import mock
+import json
+import pytest
+import requests
+import responses
+from responses import matchers
+from cloudos.clos import Cloudos
+from cloudos.utils.errors import BadRequestException
+
+APIKEY = 'vnoiweur89u2ongs'
+CLOUDOS_URL = 'http://cloudos.lifebit.ai'
+WORKSPACE_ID = 'lv89ufc838sdig'
+
+
+@mock.patch('cloudos.clos', mock.MagicMock())
+@responses.activate
+def test_get_project_list_correct_response():
+    """
+    Test 'get_project_list' to work as intended
+    """
+    params = {"teamId": WORKSPACE_ID, "apikey": APIKEY}
+    header = {
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json;charset=UTF-8"
+    }
+    search_str = f"teamId={WORKSPACE_ID}&apikey={APIKEY}"
+    # mock GET method with the .json
+    responses.add(
+            responses.GET,
+            url=f"{CLOUDOS_URL}/api/v1/projects?{search_str}",
+            headers=header,
+            match=[matchers.query_param_matcher(params)],
+            status=200)
+    # start cloudOS service
+    clos = Cloudos(apikey=APIKEY, cromwell_token=None, cloudos_url=CLOUDOS_URL)
+    # get mock response
+    response = clos.get_project_list(WORKSPACE_ID)
+    # check the response
+    assert response.status_code == 200
+    assert isinstance(response, requests.models.Response)
+
+
+@mock.patch('cloudos.clos', mock.MagicMock())
+@responses.activate
+def test_get_project_list_incorrect_response():
+    """
+    Test 'get_project_list' to fail with '400' response
+    """
+    # prepare error message
+    error_message = {"statusCode": 400, "code": "BadRequest",
+                     "message": "Bad Request.", "time": "2022-11-23_17:31:07"}
+    error_json = json.dumps(error_message)
+    params = {"teamId": WORKSPACE_ID, "apikey": APIKEY}
+    header = {
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json;charset=UTF-8"
+    }
+    search_str = f"teamId={WORKSPACE_ID}&apikey={APIKEY}"
+    # mock GET method with the .json
+    responses.add(
+            responses.GET,
+            url=f"{CLOUDOS_URL}/api/v1/projects?{search_str}",
+            body=error_json,
+            headers=header,
+            match=[matchers.query_param_matcher(params)],
+            status=400)
+    # raise 400 error
+    with pytest.raises(BadRequestException) as error:
+        # check if it failed
+        clos = Cloudos(apikey=APIKEY, cromwell_token=None, cloudos_url=CLOUDOS_URL)
+        clos.get_project_list(WORKSPACE_ID)
+    assert "Server returned status 400." in (str(error))
