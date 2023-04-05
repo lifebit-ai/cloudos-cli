e28515da6f238a0d51e171f919159da08306fd3b
dapineyro <dapineyro.dev@gmail.com>
2023-04-05 11:57:14 +0200

    update tests 2

--- tests/test_clos/test_process_project_list.py
+++ tests/test_clos/test_process_project_list.py
@@ -1,4 +1,4 @@
-"""pytest for method Cloudos.process_workflow_list"""
+"""pytest for method Cloudos.process_project_list"""
 import json
 import requests
 from cloudos.clos import Cloudos
@@ -6,9 +6,9 @@ import pytest
 import requests_mock
 import pandas as pd
 
-INPUT_JSON = "tests/test_data/process_workflow_list_initial_request.json"
-output_df = pd.read_csv("tests/test_data/process_workflow_list_results.csv")
-output_df_full = pd.read_csv("tests/test_data/process_workflow_list_results_FULL.csv")
+INPUT_JSON = "tests/test_data/projects.json"
+output_df = pd.read_csv("tests/test_data/process_project_list_results.csv")
+output_df_full = pd.read_csv("tests/test_data/process_project_list_results_FULL.csv")
 
 
 @pytest.fixture(name="mocked_requests_get")
@@ -18,19 +18,19 @@ def fixture_mocked_requests_get():
     with open(INPUT_JSON, encoding="utf-8") as json_data:
         data_d = json.load(json_data)
     with requests_mock.Mocker() as mock:
-        mock.get(f"http://test_cloud_os/api/v1/jobs?teamId={test_workspace_id}",
+        mock.get(f"http://test_cloud_os/api/v1/projects?teamId={test_workspace_id}",
                  json=data_d)
-        r_get = requests.get(f"http://test_cloud_os/api/v1/jobs?teamId={test_workspace_id}")
+        r_get = requests.get(f"http://test_cloud_os/api/v1/projects?teamId={test_workspace_id}")
     return r_get
 
 
-def test_process_workflow_list_all_fields_false(mocked_requests_get):
+def test_process_project_list_all_fields_false(mocked_requests_get):
     """Test function parameter 'all_fields=False'"""
-    df_ = Cloudos.process_workflow_list(mocked_requests_get, all_fields=False)
+    df_ = Cloudos.process_project_list(mocked_requests_get, all_fields=False)
     assert (df_.columns == output_df.columns).any()
 
 
-def test_process_workflow_list_all_fields_true(mocked_requests_get):
+def test_process_project_list_all_fields_true(mocked_requests_get):
     """Test function parameter 'all_fields=True'"""
-    df_ = Cloudos.process_workflow_list(mocked_requests_get, all_fields=True)
+    df_ = Cloudos.process_project_list(mocked_requests_get, all_fields=True)
     assert (df_.columns == output_df_full.columns).any()
