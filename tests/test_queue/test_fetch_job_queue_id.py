b5975b4e03b9a998aca03cd0b6072df8fb99c9d0
dapineyro <dapineyro.dev@gmail.com>
2023-05-02 12:54:02 +0200

    fix pytest

--- tests/test_queue/test_fetch_job_queue_id.py
+++ tests/test_queue/test_fetch_job_queue_id.py
@@ -1,9 +1,7 @@
 import mock
-import json
 import pytest
 import responses
 from cloudos.queue import Queue
-from cloudos.utils.errors import BadRequestException
 from tests.functions_for_pytest import load_json_file
 
 INPUT = 'tests/test_data/queue/queues.json'
@@ -49,10 +47,9 @@ def test_fetch_job_queue_id_batch_true_job_queue_none():
     j_queue = Queue(cloudos_url=CLOUDOS_URL, apikey=APIKEY, cromwell_token=None,
                     workspace_id=WORKSPACE_ID)
     # get mock response
-    response = j_queue.get_job_queues()
+    j_queue_id = j_queue.fetch_job_queue_id(WORKFLOW_TYPE, batch=True, job_queue=None)
     # check the response
-    assert isinstance(response, list)
-    assert len(response) == 1
+    assert j_queue_id == 'xxxxx'
 
 
 @mock.patch('cloudos.queue', mock.MagicMock())
@@ -78,10 +75,9 @@ def test_fetch_job_queue_id_batch_true_job_queue_correct():
     j_queue = Queue(cloudos_url=CLOUDOS_URL, apikey=APIKEY, cromwell_token=None,
                     workspace_id=WORKSPACE_ID)
     # get mock response
-    response = j_queue.get_job_queues()
+    j_queue_id = j_queue.fetch_job_queue_id(WORKFLOW_TYPE, batch=True, job_queue='test_queue_label')
     # check the response
-    assert isinstance(response, list)
-    assert len(response) == 1
+    assert j_queue_id == 'xxxxx'
 
 
 @mock.patch('cloudos.queue', mock.MagicMock())
@@ -108,10 +104,9 @@ def test_fetch_job_queue_id_batch_true_job_queue_wrong():
     j_queue = Queue(cloudos_url=CLOUDOS_URL, apikey=APIKEY, cromwell_token=None,
                     workspace_id=WORKSPACE_ID)
     # get mock response
-    response = j_queue.get_job_queues()
+    j_queue_id = j_queue.fetch_job_queue_id(WORKFLOW_TYPE, batch=True, job_queue='wrong_name')
     # check the response
-    assert isinstance(response, list)
-    assert len(response) == 1
+    assert j_queue_id == 'xxxxx'
 
 
 @mock.patch('cloudos.queue', mock.MagicMock())
@@ -120,26 +115,10 @@ def test_fetch_job_queue_id_batch_true_workflow_type_wrong():
     """
     Tests fetch_job_queue_id when batch=True and  wrong workflow_type is provided.
     """
-    create_json = load_json_file(INPUT)
-    header = {
-        "Accept": "application/json, text/plain, */*",
-        "Content-Type": "application/json;charset=UTF-8",
-        "apikey": APIKEY
-    }
-    # mock GET method with the .json
-    responses.add(
-        responses.GET,
-        url=f"{CLOUDOS_URL}/api/v1/teams/aws/v2/job-queues?teamId={WORKSPACE_ID}",
-        body=create_json,
-        headers=header,
-        status=200)
     # Initialise Queue
     j_queue = Queue(cloudos_url=CLOUDOS_URL, apikey=APIKEY, cromwell_token=None,
                     workspace_id=WORKSPACE_ID)
-    # get mock response
-    response = j_queue.get_job_queues()
-    # check the response
-    assert isinstance(response, list)
-    assert len(response) == 1
-
-
+    # Raise ValueError
+    with pytest.raises(ValueError) as error:
+        j_queue.fetch_job_queue_id('wrong_workflow_type', batch=True)
+    assert '[ERROR] Only nextflow or cromwell workflows are allowed' in str(error)
