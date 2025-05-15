a809e319916cff24f5bd73125d8839b864f4c97b
Juan Felipe Ortiz <juan@lifebit.ai>
2025-05-15 16:22:48 +0800

    update tests

--- tests/test_import/test_gitlab_import.py
+++ tests/test_import/test_gitlab_import.py
@@ -14,10 +14,11 @@ WF_URL_BASEGROUP = environ["WF_URL_BASEGROUP"]
 WF_URL_SUBGROUP = environ["WF_URL_SUBGROUP"]
 WF_URL_NESTEDGROUP = environ["WF_URL_NESTEDGROUP"]
 REPO_NAME = "from-gitlab-spammer-nf"
+WF_NAME = "spammer-nf"
 repo_expected_data = [
-        [WF_URL_BASEGROUP, (69676121, REPO_NAME, 89592167, "lb-ortiz")],
-        [WF_URL_SUBGROUP, (69676104, REPO_NAME, 107145926, "lb-ortiz/sample_subgroup")],
-        [WF_URL_NESTEDGROUP, (69676266, REPO_NAME, 107146238, "lb-ortiz/sample_subgroup/nested-subgroup")]
+        [WF_URL_BASEGROUP, (69676121, REPO_NAME, 21882195, "lb-ortiz")],
+        [WF_URL_SUBGROUP, (69676104, REPO_NAME, 21882195, "lb-ortiz/sample_subgroup")],
+        [WF_URL_NESTEDGROUP, (69676266, REPO_NAME, 21882195, "lb-ortiz/sample_subgroup/nested-subgroup")]
 ]
 
 
@@ -27,7 +28,7 @@ def test_fetch_correct_repo_data(repo_url, expected):
     GitlabImport = ImportGitlab(cloudos_url=CLOUDOS_URL, cloudos_apikey=CLOUDOS_TOKEN, workspace_id=WORKSPACE_ID, platform="gitlab", workflow_url=repo_url, workflow_name=REPO_NAME)
     GitlabImport.fill_payload(gitlab_apikey=GITLAB_TOKEN)
     assert GitlabImport.payload["repository"]["repositoryId"] == ex_repo_id
-    assert GitlabImport.payload["repository"]["name"] == REPO_NAME
+    assert GitlabImport.payload["repository"]["name"] == WF_NAME
     assert GitlabImport.payload["repository"]["owner"]["id"] == ex_group_id
     assert GitlabImport.payload["repository"]["owner"]["login"] == ex_group_name
 
@@ -37,7 +38,7 @@ def test_failed_login(repo_url=repo_expected_data[0][0], args=repo_expected_data
         GitlabImport = ImportGitlab(cloudos_url=CLOUDOS_URL, cloudos_apikey=CLOUDOS_TOKEN, workspace_id=WORKSPACE_ID, platform="gitlab", workflow_url=repo_url, workflow_name=REPO_NAME)
         GitlabImport.fill_payload(gitlab_apikey=GITLAB_BAD_TOKEN)
         assert GitlabImport.payload["repository"]["repositoryId"] == ex_repo_id
-        assert GitlabImport.payload["repository"]["name"] == REPO_NAME
+        assert GitlabImport.payload["repository"]["name"] == WF_NAME
         assert GitlabImport.payload["repository"]["owner"]["id"] == ex_group_id
         assert GitlabImport.payload["repository"]["owner"]["login"] == ex_group_name
     assert "Could not login to Gitlab. Check Gitlab URL and Gitlab API key" in str(excinfo.value)
