a78f5de3f71571b043614e62c7bbab3fec3337ef
Juan Felipe Ortiz <juan@lifebit.ai>
2025-06-05 17:44:37 +0800

    remove unused Github error handler

--- cloudos_cli/utils/errors.py
+++ cloudos_cli/utils/errors.py
@@ -30,25 +30,3 @@ class TimeOutException(Exception):
                "Status: {}; Reason: {}".format(rv.status_code, rv.reason))
         super(TimeOutException, self).__init__(msg)
         self.rv = rv
-
-class GithubRepositoryError(Exception):
-    """
-    Handles error codes from the Github API
-    Parameters
-    ----------
-    error_code: Error code
-    """
-    def __init__(self, error_code):
-        if error_code == 401:
-            msg = f"The user is not authorized to access this resource or resource not found (Error code: {error_code})."
-        # maybe not a good idea to trigger this in a test, as it could block user for a while
-        # Docs on the error codes here:
-        # https://docs.github.com/en/rest/using-the-rest-api/troubleshooting-the-rest-api?apiVersion=2022-11-28#rate-limit-errors
-        elif error_code in (403, 429):
-            msg = f"Access forbidden due to exceeding rate limit (Error code: {error_code})"
-        elif error_code == 404:
-            msg = f"The requested resource does not exist (Error code: {error_code})"
-        else:
-            msg = f"Unknown error code: {error_code}"
-        super(GithubRepositoryError, self).__init__(msg)
-        self.error_code = error_code
