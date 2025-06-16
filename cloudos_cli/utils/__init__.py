bdca81c8ead8991813dc3cffe83cfeeb3640e121
dapineyro <45285897+dapineyro@users.noreply.github.com>
2025-06-16 20:40:52 +0200

    Update cloudos_cli/utils/__init__.py

--- cloudos_cli/utils/__init__.py
+++ cloudos_cli/utils/__init__.py
@@ -6,5 +6,6 @@ from .errors import BadRequestException, TimeOutException, AccountNotLinkedExcep
 from .requests import retry_requests_get,  retry_requests_post, retry_requests_put
 from .resources import format_bytes, ssl_selector
 from .cloud import find_cloud
+from .cloud import find_cloud
 
 __all__ = ['errors', 'requests', 'resources', 'cloud']
