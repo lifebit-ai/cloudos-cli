d4d0ebcc1f0a2bea70d84c68df3588c341edbea9
dapineyro <45285897+dapineyro@users.noreply.github.com>
2025-06-16 20:56:08 +0200

    Update cloudos_cli/utils/__init__.py

--- cloudos_cli/utils/__init__.py
+++ cloudos_cli/utils/__init__.py
@@ -2,7 +2,7 @@
 Utility functions and classes to use across the package.
 """
 
-from .errors import BadRequestException, TimeOutException, AccountNotLinkedException, JoBNotCompletedException, NotAuthorisedException
+from .errors import BadRequestException, TimeOutException, AccountNotLinkedException, JoBNotCompletedException, NotAuthorisedException, NoCloudForWorkspaceException
 from .requests import retry_requests_get,  retry_requests_post, retry_requests_put
 from .resources import format_bytes, ssl_selector
 from .cloud import find_cloud
