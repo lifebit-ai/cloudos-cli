94baf9820587793df3c2bcc509fb4f56cc6e406b
Leila Mansouri <leila.mansouri@lifebit.ai>
2025-07-01 17:28:19 +0200

    adds new request to init.py

--- cloudos_cli/utils/__init__.py
+++ cloudos_cli/utils/__init__.py
@@ -3,7 +3,7 @@ Utility functions and classes to use across the package.
 """
 
 from .errors import BadRequestException, TimeOutException, AccountNotLinkedException, JoBNotCompletedException, NotAuthorisedException, NoCloudForWorkspaceException
-from .requests import retry_requests_get, retry_requests_post, retry_requests_put
+from .requests import retry_requests_get, retry_requests_post, retry_requests_put, retry_requests_delete
 from .resources import format_bytes, ssl_selector
 from .cloud import find_cloud
 from .cloud import find_cloud
