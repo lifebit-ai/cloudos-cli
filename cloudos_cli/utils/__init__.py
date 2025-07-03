07a17af3da46c7525636b0fbb2b321fe852ddef0
Juan Felipe Ortiz <juan@lifebit.ai>
2025-07-03 12:42:43 +0800

    review error importss

--- cloudos_cli/utils/__init__.py
+++ cloudos_cli/utils/__init__.py
@@ -2,10 +2,9 @@
 Utility functions and classes to use across the package.
 """
 
-from .errors import BadRequestException, TimeOutException, AccountNotLinkedException, JoBNotCompletedException, NotAuthorisedException, NoCloudForWorkspaceException
+from .errors import BadRequestException, TimeOutException, AccountNotLinkedException, JoBNotCompletedException, NotAuthorisedException, NoCloudForWorkspaceException, CantResumeRunningJob, CantResumeNonResumableJob, cloud_os_request_error
 from .requests import retry_requests_get, retry_requests_post, retry_requests_put, retry_requests_delete
 from .resources import format_bytes, ssl_selector
-from .cloud import find_cloud
 from .cloud import find_cloud
 from .array_job import is_valid_regex, is_glob_pattern, is_probably_regex, classify_pattern, generate_datasets_for_project, get_file_or_folder_id
 from .details import get_path
