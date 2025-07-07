58b2a88ec60f2f3a7272027f1c501bef60bf3055
Juan Felipe Ortiz <157472872+juan-lifebit@users.noreply.github.com>
2025-07-07 17:06:06 +0800

    Update cloudos_cli/__main__.py
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>

--- cloudos_cli/__main__.py
+++ cloudos_cli/__main__.py
@@ -541,9 +541,6 @@ def run(ctx,
               help=('Max time to wait (in seconds) to job completion. ' +
                     'Default=3600.'),
               default=3600)
-@click.option('--repository-platform',
-              help='Name of the repository platform of the workflow. Default=github.',
-              default='github')
 @click.option('--hpc-id',
               help=('ID of your HPC, only applicable when --execution-platform=hpc. ' +
                     'Default=660fae20f93358ad61e0104b'),
