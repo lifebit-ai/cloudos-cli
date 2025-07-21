54a7e654db3f167ce4e695f928f75121143e8fe4
Juan Felipe Ortiz <juan@lifebit.ai>
2025-07-21 12:35:39 +0800

    remove local tests

--- test_clone.sh
+++
@@ -1,5 +0,0 @@
-# Dev in Azure
-# cloudos job clone --apikey 686258e616d20bdbf2bd56cd:uqYXOV88q6vlyjpSPpucdd7dtMInkYEtID41Kuh0 --cloudos-url https://dev.sdlc.lifebit.ai --workspace-id 6480f3db916489d248956a5f --job-id 6862434e16d20bdbf2bcc30f --project-name target-id--automation-project
-
-# dev in AWS
-cloudos job clone --apikey 68625ffabea363dd863a4caa:6XGrtYVzGbux79nbUfV4bNWckVDwUPlMh7uIRBao --cloudos-url https://dev.sdlc.lifebit.ai --workspace-id 5ca489a630020c00b2fe7609 --job-id 6862855d492403c8eeeb7a1d --project-name target-id--new-report -p param1=val1 -p param2=val3 --git-tag 1.7.0
