38a5cb37583634b7bcb4902dee7e93fde6a56aaf
Daniel Boloc <danielboloc@users.noreply.github.com>
2025-08-08 17:17:21 +0200

    Update cloudos_cli/clos.py
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>

--- cloudos_cli/clos.py
+++ cloudos_cli/clos.py
@@ -544,7 +544,6 @@ class Cloudos:
                             hours = total_seconds // 3600
                             minutes = (total_seconds % 3600) // 60
                             seconds = total_seconds % 60
-                            
                             if hours > 0:
                                 return f"{hours}h {minutes}m {seconds}s"
                             elif minutes > 0:
