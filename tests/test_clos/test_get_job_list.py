bf75f7227ae38c1fded23a3417d926c316f97c7a
Daniel Boloc <danielboloc@gmail.com>
2025-09-15 13:40:32 +0200

    test: update

--- tests/test_clos/test_get_job_list.py
+++ tests/test_clos/test_get_job_list.py
@@ -21,13 +21,13 @@ def test_get_job_list_correct_response():
     API request is mocked and replicated with json files
     """
     create_json = load_json_file(INPUT)
-    params = {"teamId": WORKSPACE_ID, "archived.status": "false", "limit": 100, "page": 1}
+    params = {"teamId": WORKSPACE_ID, "archived.status": "false", "limit": 10, "page": 1}
     header = {
         "Accept": "application/json, text/plain, */*",
         "Content-Type": "application/json;charset=UTF-8",
         "apikey": APIKEY
     }
-    search_str = f"teamId={WORKSPACE_ID}&archived.status=false&limit=100&page=1"
+    search_str = f"teamId={WORKSPACE_ID}&archived.status=false&limit=10&page=1"
     # mock GET method with the .json
     responses.add(
             responses.GET,
@@ -54,13 +54,13 @@ def test_get_job_list_incorrect_response():
     error_message = {"statusCode": 400, "code": "BadRequest",
                      "message": "Bad Request.", "time": "2022-11-23_17:31:07"}
     error_json = json.dumps(error_message)
-    params = {"teamId": WORKSPACE_ID, "archived.status": "false", "limit": 100, "page": 1}
+    params = {"teamId": WORKSPACE_ID, "archived.status": "false", "limit": 10, "page": 1}
     header = {
         "Accept": "application/json, text/plain, */*",
         "Content-Type": "application/json;charset=UTF-8",
         "apikey": APIKEY
     }
-    search_str = f"teamId={WORKSPACE_ID}&archived.status=false&limit=100&page=1"
+    search_str = f"teamId={WORKSPACE_ID}&archived.status=false&limit=10&page=1"
     # mock GET method with the .json
     responses.add(
             responses.GET,
--- tests/test_errors/test_bad_request_exception.py
+++ tests/test_errors/test_bad_request_exception.py
@@ -22,13 +22,13 @@ def test_bad_request_exception():
     API request is mocked and replicated with json files
     """
     create_json = load_json_file(INPUT)
-    params = {"teamId": WORKSPACE_ID, "archived.status": "false", "limit": 100, "page": 1}
+    params = {"teamId": WORKSPACE_ID, "archived.status": "false", "limit": 10, "page": 1}
     header = {
         "Accept": "application/json, text/plain, */*",
         "Content-Type": "application/json;charset=UTF-8",
         "apikey": APIKEY
     }
-    search_str = f"teamId={WORKSPACE_ID}&archived.status=false&limit=100&page=1"
+    search_str = f"teamId={WORKSPACE_ID}&archived.status=false&limit=10&page=1"
     # mock GET method with the .json
     responses.add(
             responses.GET,
