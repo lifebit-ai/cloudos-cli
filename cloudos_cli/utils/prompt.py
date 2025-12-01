a312869c82e0e4ec685468f4f9200bff37078b16
Daniel Boloc <danielboloc@gmail.com>
2025-12-01 13:29:52 +0100

    debug

--- cloudos_cli/utils/prompt.py
+++ cloudos_cli/utils/prompt.py
@@ -2,8 +2,17 @@ import sys
 import os
 
 
-def smart_input():
+def smart_input(prompt=">>>"):
 
-    print("> ", end="", flush=True)
-    name = input()
-    return name
+    try:
+        # Try standard input()
+        return input(prompt)
+    except (EOFError, KeyboardInterrupt):
+        # Fall back to readline from stdin (for some Bash Jupyter cases)
+        try:
+            print(prompt, end="", flush=True)
+            return sys.stdin.readline().strip()
+        except Exception:
+            # If stdin is unavailable, return a default value
+            print("\n[Input unavailable, using default value]")
+            return "default_value"
