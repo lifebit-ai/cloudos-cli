db115cb65dc7dcccaf8ed57b8bfaa007a0f57506
Daniel Boloc <danielboloc@users.noreply.github.com>
2025-12-15 11:40:54 +0100

    Apply suggestions from code review
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>

--- cloudos_cli/jobs/job.py
+++ cloudos_cli/jobs/job.py
@@ -1140,7 +1140,7 @@ class Job(Cloudos):
 
         # Override branch if provided
         # sometimes revision is missing from the 'request-payload' API, make sure is present
-        if 'revision' not in cloned_payload or not cloned_payload.get('revision'):
+        if 'revision' not in cloned_payload:
             revision = self.get_field_from_jobs_endpoint(source_job_id, field="revision", verify=verify)
             cloned_payload['revision'] = revision
             if 'digest' in cloned_payload['revision']:
