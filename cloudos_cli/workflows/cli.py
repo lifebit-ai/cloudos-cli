786f6e64dfb020d27d5302d86bb3caf3933b0525
Daniel Boloc <danielboloc@users.noreply.github.com>
2026-04-27 09:09:13 +0200

    Update cloudos_cli/workflows/cli.py
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>

--- cloudos_cli/workflows/cli.py
+++ cloudos_cli/workflows/cli.py
@@ -83,7 +83,7 @@ def list_workflows(ctx,
         print('\t...Preparing objects')
     cl = Cloudos(cloudos_url, apikey, None)
     if verbose:
-        print('\tThe following Lifebit Platform object was created:')
+        print('\tThe following Cloudos object was created:')
         print('\t' + str(cl) + '\n')
         print('\tSearching for workflows in the following workspace: ' +
               f'{workspace_id}')
