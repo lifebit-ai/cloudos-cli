c0eba7b20e95388ccb48570755a0251456724249
Daniel Boloc <danielboloc@users.noreply.github.com>
2026-04-27 09:09:27 +0200

    Update cloudos_cli/cromwell/cli.py
    Co-authored-by: dapineyro <45285897+dapineyro@users.noreply.github.com>

--- cloudos_cli/cromwell/cli.py
+++ cloudos_cli/cromwell/cli.py
@@ -129,7 +129,7 @@ def cromwell_restart(ctx,
         print('\t...Preparing objects')
     cl = Cloudos(cloudos_url, apikey, cromwell_token)
     if verbose:
-        print('\tThe following Lifebit Platform object was created:')
+        print('\tThe following Cloudos object was created:')
         print('\t' + str(cl) + '\n')
         print(f'\tStarting Cromwell server in {workspace_id} workspace')
     cl.cromwell_switch(workspace_id, action, verify_ssl)
